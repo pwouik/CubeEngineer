87c70e0b4e6fa9210bf17afd8ba71f25f1cc5592
ChristopheCai <caichristo@cy-tech.fr>
2025-04-15 14:37:12 +0200

    Setup Grid

--- Assets/Scripts/BlockGrid.cs
+++ Assets/Scripts/BlockGrid.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -10,27 +11,32 @@ struct Element{
 
 public class BlockGrid : MonoBehaviour
 {
-    [SerializeField]private List<GameObject> blockPrefabs;
-    private Element[,,] grid = new Element[32, 32, 32];
+    [SerializeField] private List<GameObject> blockPrefabs;
+    [SerializeField] private int size = 16;
+    private Element[,,] grid;
     // Start is called before the first frame update
     void Start()
     {
-        for(int x = 0;x<32;x++)
-        for(int y = 0;y<32;y++)
-        for(int z = 0;z<32;z++){
-            grid[z,y,x] = new Element()
-            {
-                type = (short)Random.Range(0,blockPrefabs.Count),
-                orientation = 0,
-                rotation = 0,
-            };
-            Instantiate(blockPrefabs[grid[z,y,x].type], new Vector3(x, y, z), Quaternion.identity);
-        }
+        grid = new Element[size, size, size];
+        float scale = blockPrefabs[0].transform.localScale.x;
+        for (int x = 0; x < size; x++)
+            for (int y = 0; y < size; y++)
+                for (int z = 0; z < size; z++)
+                {
+                    grid[z, y, x] = new Element()
+                    {
+                        type = (short)Math.Max(0, UnityEngine.Random.Range(0, blockPrefabs.Count + 16) - 16),
+                        orientation = 0,
+                        rotation = 0,
+                    };
+                    if (grid[z, y, x].type != 0)
+                        Instantiate(blockPrefabs[grid[z, y, x].type], new Vector3(x + .5f, y + .5f, z + .5f) * scale + transform.position, Quaternion.identity, transform);
+                }
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
     }
 }
